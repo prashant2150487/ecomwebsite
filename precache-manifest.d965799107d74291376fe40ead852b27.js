6056c4ae3a070bfe18b016e8f1fc230c98da902e
prashant2150487 <you@example.com>
2022-10-21 04:55:31 +0530

    Updates

---
+++ precache-manifest.d965799107d74291376fe40ead852b27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dd21056108c5213eeed",
+    "url": "/ecomwebsite/static/css/main.f1afcfb8.chunk.css"
+  },
+  {
+    "revision": "1dd21056108c5213eeed",
+    "url": "/ecomwebsite/static/js/main.1dd21056.chunk.js"
+  },
+  {
+    "revision": "af0aac77d8aecc35095c",
+    "url": "/ecomwebsite/static/js/1.af0aac77.chunk.js"
+  },
+  {
+    "revision": "bc2a1b1dd754e75ec1e9",
+    "url": "/ecomwebsite/static/js/2.bc2a1b1d.chunk.js"
+  },
+  {
+    "revision": "6c5872904a23014c40d0",
+    "url": "/ecomwebsite/static/js/runtime~main.6c587290.js"
+  },
+  {
+    "revision": "03675cb62b7a92a1bb17e730ce0f0165",
+    "url": "/ecomwebsite/index.html"
+  }
+];
